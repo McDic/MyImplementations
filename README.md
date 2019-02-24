922b2eb0504b099966a5cf98453a94fc73ab94d4
McDic <spongbob9876@naver.com>
2019-02-24 17:53:42 +0900

    Corrected EoS

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ All source codes are available on `/src` folder.
 
 Currently implemented:
 
+- Math
+	- Sieve of Eratosthenes
+	- Primes sqrt(n) expanding
+
 - Graph
 	- Base undirected graph template
 	- DFS, BFS
--- src/eratosthenes_sieve.cpp
+++ src/eratosthenes_sieve.cpp
@@ -1,45 +1,38 @@
 /*
 	Author: McDic
-	Description: Implementation of sieve of Eratosthenes.
+	Description: Implementation of SoE(Sieve of Eratosthenes).
 */
 
 #include <stdio.h>
 #include <assert.h>
 #include <vector>
 
-// Constants
-typedef long long int lld;
-
 // Attributes
-std::vector<bool> isPrime = {false, false, true};
-std::vector<lld> primes = {2};
-
-// Expand isPrime by SoE
-void expand(const int limit){
-	while(isPrime.size() < limit){
-		isPrime.push_back(true);
-		lld num = isPrime.size() - 1;
-		for(auto p: primes){
-			if(p*p > num) break;
-			else if(num%p == 0){
-				isPrime[num] = false;
-				break;
-			}
-		}
-		if(isPrime[num]) primes.push_back(num);
-	}
-}
+std::vector<bool> isPrime;
+std::vector<int> primes;
 
 int main(void){
 	
 	// Get input
 	printf("Input upper bound to calculate all primes: ");
-	int n; scanf("%d", &n); assert(1<n);
+	int n; scanf("%d", &n);
+	assert(2<=n);
 	
-	// Expand and show result
-	expand(n);
+	// Sieve of Eratosthenes
+	primes.clear();
+	isPrime = std::vector<bool>(n+1, true);
+	isPrime[0] = false; isPrime[1] = false;
+	for(int i=2; i<=n; i++){
+		if(isPrime[i]){
+			for(int j=2*i; j<=n; j+=i) isPrime[j] = false;
+			primes.push_back(i);
+		}
+	}
+	
+	// Show result
 	printf("Primes under %d: ", n); 
-	for(auto p: primes) printf("%lld ", p);
+	for(auto p: primes) printf("%d ", p);
 	printf("\n");
+	
 	return 0;
 }
