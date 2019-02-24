99211c3b520e8f9a17d9191445ef4ee43c72c2e6
McDic <spongbob9876@naver.com>
2019-02-24 18:26:48 +0900

    Added backtrack

--- src/dijkstra.cpp
+++ src/dijkstra.cpp
@@ -1,6 +1,6 @@
 /*
 	Author: McDic
-	Description: Implementation of dijkstra algorithm for undirected graph.
+	Description: Implementation of dijkstra algorithm (with shortest track) for undirected graph.
 */
 
 #include <stdio.h>
@@ -33,6 +33,7 @@ int main(void){
 	// Construct PQ
 	lld inf_dist = -1LL ^ (1LL<<63);
 	std::vector<lld> dist(v+1, inf_dist); dist[start] = 0;
+	std::vector<int> backtrack(v+1, -1);
 	typedef std::pair<lld, int> pli;
 	std::priority_queue<pli, std::vector<pli>, std::greater<pli>> pq;
 	pq.push({0, start});
@@ -44,6 +45,7 @@ int main(void){
 		for(auto next_info: edges[now]){
 			int next = next_info.first, singledist = next_info.second;
 			if(dist[next] > dist[now] + singledist){
+				backtrack[next] = now;
 				dist[next] = dist[now] + singledist;
 				pq.push({dist[next], next});
 			}
@@ -51,7 +53,17 @@ int main(void){
 	}
 	
 	// Print
-	for(int i=1; i<=v; i++) printf("Distance between #%d ~ #%d: %lld\n", start, i, dist[i]);
+	for(int i=1; i<=v; i++){
+		if(dist[i] == inf_dist){
+			printf("#%d is impossible to reach\n", i);
+			continue;
+		}
+		printf("Distance between #%d ~ #%d: %lld / ", start, i, dist[i]);
+		std::vector<int> track; track.push_back(i);
+		while(backtrack[track.back()] != -1) track.push_back(backtrack[track.back()]);
+		printf("Track: %d", track.back());
+		for(int j=track.size()-2; j>=0; j--) printf(" -> %d", track[j]); printf("\n");
+	}
 	
 	return 0;
 }
