4c30ea02b625cf2e12af3705c19b755c65a444a2
McDic <spongbob9876@naver.com>
2019-02-24 17:45:43 +0900

    Published Sieve of Eratosthenes

---
+++ src/eratosthenes_sieve.cpp
@@ -0,0 +1,45 @@
+/*
+	Author: McDic
+	Description: Implementation of sieve of Eratosthenes.
+*/
+
+#include <stdio.h>
+#include <assert.h>
+#include <vector>
+
+// Constants
+typedef long long int lld;
+
+// Attributes
+std::vector<bool> isPrime = {false, false, true};
+std::vector<lld> primes = {2};
+
+// Expand isPrime by SoE
+void expand(const int limit){
+	while(isPrime.size() < limit){
+		isPrime.push_back(true);
+		lld num = isPrime.size() - 1;
+		for(auto p: primes){
+			if(p*p > num) break;
+			else if(num%p == 0){
+				isPrime[num] = false;
+				break;
+			}
+		}
+		if(isPrime[num]) primes.push_back(num);
+	}
+}
+
+int main(void){
+	
+	// Get input
+	printf("Input upper bound to calculate all primes: ");
+	int n; scanf("%d", &n); assert(1<n);
+	
+	// Expand and show result
+	expand(n);
+	printf("Primes under %d: ", n); 
+	for(auto p: primes) printf("%lld ", p);
+	printf("\n");
+	return 0;
+}
