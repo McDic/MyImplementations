a7abfc66eecc80c92ff7a884114f349df2a12e53
McDic <spongbob9876@naver.com>
2019-03-03 15:20:21 +0900

    Tweaked output

--- src/eratosthenes_sieve.cpp
+++ src/eratosthenes_sieve.cpp
@@ -15,7 +15,7 @@ int main(void){
 	
 	// Get input
 	printf("Input upper bound to calculate all primes: ");
-	int n; scanf("%d", &n);
+	int n = 20000000; scanf("%d", &n);
 	assert(2<=n);
 	
 	// Sieve of Eratosthenes
@@ -30,8 +30,8 @@ int main(void){
 	}
 	
 	// Show result
-	printf("Primes under %d: ", n); 
-	for(auto p: primes) printf("%d ", p);
+	printf("Total %d primes under %d (showing only first 1000th): ", primes.size(), n); 
+	for(int i=0; i<1000 && i<primes.size(); i++) printf("%d ", primes[i]);
 	printf("\n");
 	
 	return 0;
