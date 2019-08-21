32188b73498b8b707ca2c0a2cb243a965227ceb7
McDic <spongbob9876@naver.com>
2019-08-21 09:41:44 +0900

    Made class name uppercase for first letter

--- src/big_integer.cpp
+++ src/big_integer.cpp
@@ -9,14 +9,14 @@
 #include <string>
 
 // Big integer class
-class bigInt{
+class BigInt{
 private:
 	
 	// ----------------------------------------------------------------------------------
 	// Attributes
 	
 	bool sign; // + or -. Zero is considered as positive.
-	std::vector<int> digits; // Each digit stores 10^4.
+	std::vector<int> digits; // Each digit stores 10^4. Eariler index stores lower digit area.
 	const static int onesize = 10000;
 	
 	// ----------------------------------------------------------------------------------
@@ -34,8 +34,8 @@ public:
 	// Constructors
 	
 	// Constructor by int
-	bigInt(int x = 0){*this = bigInt((long long int)x);}
-	bigInt(long long int x){
+	BigInt(int x = 0){*this = BigInt((long long int)x);}
+	BigInt(long long int x){
 		if(x>=0) sign = true;
 		else x *= -1, sign = false;
 		digits.clear();
@@ -46,8 +46,8 @@ public:
 	}
 	
 	// Constructor by string
-	bigInt(const char* &line){*this = bigInt(std::string(line));}
-	bigInt(std::string line){
+	BigInt(const char* &line){*this = BigInt(std::string(line));}
+	BigInt(std::string line){
 		if(line[0] == '+' || ('0' <= line[0] && line[0] <= '9')) sign = true;
 		else sign = false;
 		digits.clear();
@@ -64,9 +64,9 @@ public:
 		} cleanBack();
 	}
 	
-	// Constructor by bigInt
-	bigInt(const bigInt &another){
-		digits = another.digits;
+	// Constructor by BigInt
+	BigInt(const BigInt &another){
+		*this = another;
 		cleanBack();
 	}
 
@@ -81,16 +81,16 @@ public:
 	}
 	
 	// ----------------------------------------------------------------------------------
-	// Comparing operators: It can be optimized even more, but for convenience of study I don't implemented it.
+	// Comparing operators: It can be optimized even more, but I don't implemented it yet.
 	
-	bool operator == (const bigInt &another){ // this == another ?
+	bool operator == (const BigInt &another){ // this == another ?
 		if(digits.empty() && another.digits.empty()) return true;
 		else if(sign != another.sign || digits.size() != another.digits.size()) return false;
-		for(int i=0; i<digits.size(); i++) if(digits[i] != another.digits[i]) return false;
+		for(int i=0; i<(int)digits.size(); i++) if(digits[i] != another.digits[i]) return false;
 		return true;
 	}
-	bool operator != (const bigInt &another){return !(this->operator ==(another));}
-	bool operator > (const bigInt &another){ // this > another ?
+	bool operator != (const BigInt &another){return !(this->operator ==(another));}
+	bool operator > (const BigInt &another){ // this > another ?
 		if(sign != another.sign) return sign; // this < 0 <= another (sign = false) || another < 0 <= this (sign = true)
 		else if(digits.size() > another.digits.size()) return sign; // abs(this) > abs(another)
 		else if(digits.size() < another.digits.size()) return !sign; // abs(this) < abs(another)
@@ -99,41 +99,41 @@ public:
 			else if(digits[i] < another.digits[i]) return !sign; // abs(another) > abs(this)
 		} return false; // Absolute same
 	}
-	bool operator >= (const bigInt &another){return this->operator ==(another) || this->operator >(another);}
-	bool operator < (const bigInt &another){return !(this->operator >=(another));}
-	bool operator <= (const bigInt &another){return !(this->operator >(another));}
+	bool operator >= (const BigInt &another){return this->operator ==(another) || this->operator >(another);}
+	bool operator < (const BigInt &another){return !(this->operator >=(another));}
+	bool operator <= (const BigInt &another){return !(this->operator >(another));}
 	
 	// ----------------------------------------------------------------------------------
 	// Arithmetic operator
 	
-	bigInt operator -() const { // return -x
-		bigInt result(*this); result.sign = !(result.sign);
+	BigInt operator -() const{ // return -x
+		BigInt result(*this); result.sign = !(result.sign);
 		return result;
 	}
-	bigInt operator +() const {return *this;} // return +a (= a)
+	BigInt operator +() const {return *this;} // return +a (= a)
 	
-	bigInt operator +(const bigInt &another){ // return this + another
+	BigInt operator +(const BigInt &another){ // return this + another
 		if(sign != another.sign) return this->operator -(-another);
-		bigInt result(0);
+		BigInt result(0);
 		int maxdigitsize = (digits.size() > another.digits.size() ? digits.size() : another.digits.size());
 		result.digits = std::vector<int>(0, maxdigitsize + 1);
 		for(int i=0; i < maxdigitsize; i++){
-			if(i < digits.size()) result.digits[i] += digits[i];
-			if(i < another.digits.size()) result.digits[i] += another.digits[i];
-			result[i+1] += result[i] / onesize;
-			result[i] %= onesize;
+			if(i < (int)digits.size()) result.digits[i] += digits[i];
+			if(i < (int)another.digits.size()) result.digits[i] += another.digits[i];
+			result.digits[i+1] += result.digits[i] / onesize;
+			result.digits[i] %= onesize;
 		} result.cleanBack();
 		return result;
 	}
-	bigInt operator -(const bigInt &another){ // return this - another
+	BigInt operator -(const BigInt &another){ // return this - another
 		if(sign != another.sign) return this->operator +(-another);
 	}
 };
 
 int main(void){
 	
-	bigInt a("1725543529834737");
-	bigInt b("941515007841");
+	BigInt a("1725543529834737");
+	BigInt b("941515007841");
 	std::cout << a.str() << " == " << b.str() << " = " << (a==b ? "TRUE":"FALSE") << std::endl;
 	std::cout << a.str() << " > " << b.str() << " = " << (a>b ? "TRUE":"FALSE") << std::endl;
 	std::cout << a.str() << " < " << b.str() << " = " << (a<b ? "TRUE":"FALSE") << std::endl;
